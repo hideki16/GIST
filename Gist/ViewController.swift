d162f03c0d60db2d02c804f20e811cb076f4b400
gabriel hideki <gabriel_shippo@hotmail.com>
2022-10-19 15:16:21 -0300

    Initial Commit

---
+++ Gist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gist
+//
+//  Created by gabriel hideki on 19/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
